32b7aa9306cbcc72b2f93553b086f8fd3c518255
vaibhavar <webdevpls@gmail.com>
2018-06-10 17:25:37 +0530

    Added intial template for library

--- app/index.js
+++ app/index.js
@@ -5,8 +5,81 @@ module.exports = class extends Generator {
 		// Calling the super constructor
 		super(args, opts);
 
+		this.answers = {};
+
 		// work half done
-		this.log('Yo UI5Lab! \n\n "Well begun is half done"- Aristotle');
+		this.log('Yo UI5Lab! \n\n "Well begun is half done"- Aristotle \n');
+	}
+
+	prompting() {
+		return this.prompt([
+			{
+				type: 'input',
+				name: 'libraryName',
+				message: 'Your library name',
+				default: 'ui5lab',
+			},
+			{
+				type: 'input',
+				name: 'controlName',
+				message: 'Your control name',
+			},
+		]).then(
+			function(answers) {
+				this.answers = answers;
+				this.log(answers);
+			}.bind(this)
+		);
+	}
+
+	writing() {
+		const sLibName = this.answers.libraryName;
+		const sControlName = this.answers.controlName;
+		const sDir = sLibName.split('.').join('/');
+
+		this.log('\n\n Buzzing the engines');
+		// Copy library file
+		this.fs.copyTpl(
+			this.templatePath('library.js'),
+			this.destinationPath(`${sDir}/library.js`),
+			{
+				libraryName: sLibName,
+				controlName: sControlName,
+			}
+		);
+
+		// Copy Control file
+		this.fs.copyTpl(
+			this.templatePath('Control.js'),
+			this.destinationPath(`${sDir}/${sControlName}.js`),
+			{
+				libraryName: sLibName,
+				controlName: sControlName,
+			}
+		);
+
+		this.fs.copyTpl(
+			this.templatePath('ControlRenderer.js'),
+			this.destinationPath(`${sDir}/${sControlName}Renderer.js`),
+			{
+				libraryName: sLibName,
+				controlName: sControlName,
+			}
+		);
+
+		this.fs.copyTpl(
+			this.templatePath('Control.js'),
+			this.destinationPath(`${sDir}/${sControlName}.js`),
+			{
+				libraryName: sLibName,
+				controlName: sControlName,
+			}
+		);
+
+		this.fs.copyTpl(this.templatePath('demo.html'), this.destinationPath(`demo.html`), {
+			libraryName: sLibName,
+			controlName: sControlName,
+		});
 	}
 
 	createComponent() {
@@ -14,6 +87,6 @@ module.exports = class extends Generator {
 	}
 
 	addDocumentation() {
-		this.log('Coming soon: Documentation for next steps & contribution to UI5Lab');
+		// this.log('Coming soon: Documentation for next steps & contribution to UI5Lab');
 	}
 };
